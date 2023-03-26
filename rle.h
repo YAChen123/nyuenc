3527f1eeb135063629083de1f1bb3087a7bc59bf
YAChen123 <andychen123583@gmail.com>
2023-03-26 15:49:45 -0400

    add document

--- rle.h
+++ rle.h
@@ -1,7 +1,6 @@
 #ifndef _RLE_H_
 #define _RLE_H_
 
-// some function
 int get_num_thread(int argc, char **argv);
 int sequential(char *addr, size_t size);
 int parallel(int jobs, char *addr, size_t size);
